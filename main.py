9a5ccbbbfc8f06dfa176de22347b7c2f1589b61f
Mohammad Khazaei <mkhazaei.dev@gmail.com>
2024-08-20 16:54:06 +0330

    edit Main class in main.py

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-import csv
 from database import DBConnection
+from models import Contact
 
 
 class Main:
     def __init__(self):
-        self.db = DBConnection("Contacts.csv")
-
-    def show_contacts(self):
-        with self.db as db:
-            for contact in csv.reader(db):
-                print(contact)
+        self.db = DBConnection("Contacts.csv", Contact.get_csv_header())
 
 
 if __name__ == "__main__":
     main = Main()
-    main.show_contacts()
