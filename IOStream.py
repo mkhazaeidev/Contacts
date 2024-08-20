d16dd4decfff1b8a3acbe9dbf2860bbd60568978
Mohammad Khazaei <mkhazaei.dev@gmail.com>
2024-08-20 13:30:51 +0330

    Completion of IOStream class

--- IOStream.py
+++ IOStream.py
@@ -1,3 +1,6 @@
+import hashlib
+from getpass import getpass
+
 from validations import Validations
 
 
@@ -10,3 +13,44 @@ class IOStream:
                 return name
             else:
                 print("Please enter a valid name.")
+
+    @staticmethod
+    def get_email(prompt: str) -> str:
+        while True:
+            email = input(prompt)
+            if Validations.is_email(email):
+                return email
+            else:
+                print("Please enter a valid email.")
+
+    @staticmethod
+    def get_cellphone(prompt: str) -> str:
+        while True:
+            cellphone = input(prompt)
+            if Validations.is_cellphone(cellphone):
+                return cellphone
+            else:
+                print("Please enter a valid cellphone number.")
+
+    @staticmethod
+    def get_landline(landline: str) -> str:
+        while True:
+            landline = input(landline)
+            if Validations.is_landline(landline):
+                return landline
+            else:
+                print("Please enter a valid landline number.")
+
+    @staticmethod
+    def hash_password(password: str) -> str:
+        return hashlib.sha256(password.encode("UTF-8")).hexdigest()
+
+    @staticmethod
+    def get_password(prompt: str) -> str:
+        while True:
+            password = getpass(prompt)
+            if Validations.is_password(password):
+                return IOStream.hash_password(password)
+            else:
+                print("Please enter a valid password.")
+
