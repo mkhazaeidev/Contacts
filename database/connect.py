5c17d619cfe12f479a68bd2d34899f836b5eb76d
Mohammad Khazaei <mkhazaei.dev@gmail.com>
2024-08-20 16:36:17 +0330

    Add a header attribute to the DBConnection.

--- database/connect.py
+++ database/connect.py
@@ -2,14 +2,16 @@ from pathlib import Path
 
 
 class DBConnection:
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: str, headers: str):
         self.file_path = file_path
+        self.headers = headers
         self.connection = None
 
     def __enter__(self):
         path = Path(self.file_path)
         if not path.is_file():
             file = open(str(path), mode='w', encoding='UTF8')
+            file.write(self.headers)
             file.close()
 
         self.connection = open(str(path), mode='r', encoding='UTF8')
